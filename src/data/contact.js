41fb9c661610573684c0271b81129e7f804d9b37
SagarSave10 <ssave@umich.edu>
2025-11-18 23:43:53 -0500

    Remove unused icon imports from contact.js
    Removed unused imports for GitHub and Instagram icons.

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,5 +1,4 @@
-import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
+
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
