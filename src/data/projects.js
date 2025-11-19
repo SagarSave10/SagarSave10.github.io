f32f79cb58b3f4f451263f2ec98a497d68bc15d5
SagarSave10 <ssave@umich.edu>
2025-11-19 00:43:14 -0500

    Refactor project entries in projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,14 @@
 // TODO Add a couple lines about each project
 const data = [
-    {
+  {
   title: 'Analysis of Active Thermal Pre-Conditioning on EV Battery SOH in Hot Climates',
   subtitle: 'Lithium Battery Lifecycle Management | University of Michigan',
   image: '/images/batterypreconditiong.jpg',
   date: '2025-11-24',
   desc:
     'This project involved designing and simulating an active Battery Thermal Management System (BTMS) in MATLAB/Simulink to prevent heat soaking and degradation in EV batteries during key-off (parked) conditions, particularly in hot climates where ambient temperatures peak near 45°C. The core achievement was the development of a Simulink model coupling a 2-state thermal RC battery model with a closed-loop 3-mode controller for the cooling system. This controller intelligently switches the system ON or OFF based on internal temperature thresholds, ensuring the battery core stays below 30°C. The model also simulates a fixed-power A/C compressor by generating two outputs: a thermal sink signal (Q_cooler) based on the COP, and a parasitic electrical load (I_cooler = P/V) that discharges the battery. This enables accurate estimation of the energy penalty required to maintain thermal stability.',
-    },
-    {
+  },
+  {
     title: 'High Sensitivity Comb-Drive Capacitive Accelerometer for Head-Mounted Display',
     subtitle: 'EECS 425: Integrated Microsystems | University of Michigan',
     link: 'https://drive.google.com/file/d/1RhFZGr_jvjAV5osILd_MIghXXdhnywEo/view?usp=sharing',
@@ -16,16 +16,16 @@ const data = [
     date: '2025-04-21',
     desc:
       'Designed, simulated, and tested a high-sensitivity capacitive MEMS accelerometer for AR/VR head-mounted displays. The device featured an asymmetrical comb-drive with a variable-gap design, achieving 0.13 pF/g sensitivity over a ±20 g range. Fabricated using a Silicon-on-Glass process and integrated with a custom CMOS readout circuit, the system delivered 47 mV/g sensitivity and ~130 µg resolution. Structural and capacitance simulations were conducted in COMSOL and SolidWorks; circuit validation was performed in Cadence. The system supports electrostatic force testing and offers a tunable bandwidth up to 30 Hz—ideal for low-latency motion tracking in wearable electronics.',
-    },
-    {
+  },
+  {
     title: 'Design for Automated Installation of Vehicle Wiring Systems',
     subtitle: 'Graduate Research',
     image: '/images/projects/DFA.jpg',
     date: '2025-06-17',
     desc:
       'This project focused on automating the manual and ergonomically challenging process of wire harness installation in vehicle assembly using Design for Automated Robotic Assembly (DFARA) principles. A quantitative methodology was developed to assess robotic feasibility based on geometric and functional complexity. Key contributions include a 7-digit complexity framework to evaluate handling and insertion difficulty, a cost model linking design metrics to robotic operation expenses, and an ontology-based task structure to rank installation steps by robotic difficulty. Design recommendations—such as modular harnesses, selective stiffness zones, and automation-friendly connectors—were proposed to improve robotic compatibility. The approach also incorporated product family design strategies for scalable automation across vehicle platforms, laying a foundation for AI-driven, vision-assisted robotic systems. ',
-    },
-    {
+  },
+  {
     title: 'Analysis of Domestic Supply Chains and Manufacturing Capability',
     subtitle: 'Consulting project with Centrepolis Accelerators.',
     link: 'https://drive.google.com/file/d/17BFl6ujLWJzfu0zlI_UwXPNjm2YdpxPv/view?usp=sharing',
@@ -33,8 +33,8 @@ const data = [
     date: '2024-12-10',
     desc:
       'This project focused on strengthening U.S. supply chains in battery manufacturing, semiconductors, electric motors, and casting/forging. The team developed data-driven roadmaps and an interactive Power BI dashboard profiling 180+ companies, with insights on financials, investors, and geographic distribution. Key deliverables included supplier profiling, transportation footprint mapping, and identification of bottlenecks. The project resulted in 30+ high-potential investment opportunities and a comprehensive technology-policy report for OEMs, startups, and coalitions, enabling informed decision-making and regional development. ',
-    },
-    {
+  },
+  {
     title: 'Design and Simulation of Biomimetic Flapping Wing Micro Aerial Vehicle (MAV)',
     subtitle: 'Undergraduate Final Year Project',
     link: 'https://drive.google.com/file/d/1m6wh9FkM7CHxseNdj9mkaHhYd53PUu9R/view?usp=drive_link',
@@ -43,7 +43,7 @@ const data = [
     desc:
       'Designed and simulated a flapping-wing MAV inspired by insect flight, focusing on stable hovering and lift generation. Conducted transient CFD analysis using ANSYS FLUENT to evaluate unsteady aerodynamic performance, with structural modeling in SolidWorks.'
       + 'Achievements: Secured 2nd place in Innovation Next Projects and ranked among the top 10 finalists in a university-level research paper competition for innovation and technical depth.',
-    },
+  },
 ];
 
 export default data;
