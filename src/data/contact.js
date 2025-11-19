89a7d89f0707452453db1e7be5ac21c6aecd0741
SagarSave10 <ssave@umich.edu>
2025-11-18 23:54:57 -0500

    Fix formatting in contact.js

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,15 +1,13 @@
-
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
 
 const data = [
-    {
+  {
     link: 'https://www.linkedin.com/in/sagar-save1',
     label: 'LinkedIn',
     icon: faLinkedinIn,
   },
-
 ];
 
 export default data;
